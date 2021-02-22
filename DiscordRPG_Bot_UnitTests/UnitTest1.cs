a575959d4eb1b54776f891c5f79eb234211da12c
Marius Kristiansen <thewisetorsk@gmail.com>
2021-02-22 15:32:05 +0100

    Added Website project and UnitTesting projects

---
+++ DiscordRPG_Bot_UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DiscordRPG_Bot_UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
