1ce460d4bf53578aeb677ca9eafb2eb945cfef02
Marius Kristiansen <thewisetorsk@gmail.com>
2021-02-23 10:19:35 +0100

    Change to MVC

--- DiscordRPG_WebInterface/Program.cs
+++ DiscordRPG_WebInterface/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DiscordRPG_WebInterface
+namespace DiscordRPG_Webinterface
 {
     public class Program
     {
