c166331595030d2ab6f8a0807740c60e3b6dd38c
popandepo <popandepo@gmail.com>
2021-02-23 08:18:31 +0100

    Added (nonfuntioning) bot initiator

--- Program.cs
+++ Program.cs
@@ -1,12 +1,65 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
 
 namespace DiscordRPG
 {
     class Program
     {
+        public static DiscordSocketClient _client;
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            
+        }
+
+        /// <summary>
+        /// Initiates the bot
+        /// </summary>
+        /// <returns>Nothing</returns>
+        private async Task BotInit() //starts the bot and initiates all handlers
+        {
+            _client = new DiscordSocketClient(new DiscordSocketConfig() { AlwaysDownloadUsers = true });
+            string token = "ODAxMDU1NDY1ODUyMjM5ODcy.YAbHCQ.J61LcyB49Nf2OSK2PfsrNQp2w-k";
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+            await _client.SetStatusAsync(UserStatus.Online);
+
+            _client.MessageReceived += MessageHandler;//Whenever a message is heard, push it to the message handler
+            _client.ReactionAdded += ReactionHandler;//Whenever a reaction is added, push it to the reaction handler
+            _client.ReactionRemoved += ReactionHandler;//Whenever a reaction is removed, push it to the reaction handler
+        }
+
+        /// <summary>
+        /// Deals with Reactions
+        /// </summary>
+        /// <param name="trash1">Useless but needed</param>
+        /// <param name="channel">The channel the reaction comes from</param>
+        /// <param name="reaction">The reaction</param>
+        /// <returns>Task.CompletedTask</returns>
+        private Task ReactionHandler(Cacheable<IUserMessage, ulong> trash1, ISocketMessageChannel channel, SocketReaction reaction)
+        {
+            var emote = reaction.Emote;
+            var message = reaction.Message.Value;
+            var reacter = reaction.User.Value;
+            var author = reaction.Message.Value.Author;
+
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Deals with Messages
+        /// </summary>
+        /// <param name="message">The message that was sent</param>
+        /// <returns>Task.CompletedTask</returns>
+        private Task MessageHandler(SocketMessage message)
+        {
+            var author = message.Author;
+            var channel = message.Channel;
+
+            return Task.CompletedTask;
         }
     }
 }
