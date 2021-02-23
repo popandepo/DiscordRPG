d954f7bfdce91ac4af4a2cbc0cdc672e9ca5709e
popandepo <popandepo@gmail.com>
2021-02-23 09:13:22 +0100

    Bot is correctly launching now

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Discord;
 using Discord.WebSocket;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace DiscordRPG
@@ -9,19 +10,30 @@ namespace DiscordRPG
     {
         public static DiscordSocketClient _client;
         
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("test");
+            await BotInit();
+            Console.ReadLine();
         }
 
         /// <summary>
         /// Initiates the bot
         /// </summary>
         /// <returns>Nothing</returns>
-        private async Task BotInit() //starts the bot and initiates all handlers
+        private static async Task BotInit() //starts the bot and initiates all handlers
         {
             _client = new DiscordSocketClient(new DiscordSocketConfig() { AlwaysDownloadUsers = true });
-            string token = "";
+
+            var filePath = Path.GetFullPath("BotKey.txt");
+
+            Console.WriteLine("File path gotten: " + filePath);
+
+            string token = File.ReadAllText(filePath);
+
+            Console.WriteLine("Token gotten: " + token);
+            
+
 
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
@@ -30,6 +42,8 @@ namespace DiscordRPG
             _client.MessageReceived += MessageHandler;//Whenever a message is heard, push it to the message handler
             _client.ReactionAdded += ReactionHandler;//Whenever a reaction is added, push it to the reaction handler
             _client.ReactionRemoved += ReactionHandler;//Whenever a reaction is removed, push it to the reaction handler
+            
+            Console.WriteLine("Initiated!");
         }
 
         /// <summary>
@@ -39,7 +53,7 @@ namespace DiscordRPG
         /// <param name="channel">The channel the reaction comes from</param>
         /// <param name="reaction">The reaction</param>
         /// <returns>Task.CompletedTask</returns>
-        private Task ReactionHandler(Cacheable<IUserMessage, ulong> trash1, ISocketMessageChannel channel, SocketReaction reaction)
+        private static Task ReactionHandler(Cacheable<IUserMessage, ulong> trash1, ISocketMessageChannel channel, SocketReaction reaction)
         {
             var emote = reaction.Emote;
             var message = reaction.Message.Value;
@@ -54,7 +68,7 @@ namespace DiscordRPG
         /// </summary>
         /// <param name="message">The message that was sent</param>
         /// <returns>Task.CompletedTask</returns>
-        private Task MessageHandler(SocketMessage message)
+        private static Task MessageHandler(SocketMessage message)
         {
             var author = message.Author;
             var channel = message.Channel;
