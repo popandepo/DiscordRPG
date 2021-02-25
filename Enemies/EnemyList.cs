0e3e7c3a87598c55c089103e6daae4f36d9a71ee
popandepo <popandepo@gmail.com>
2021-02-25 18:36:52 +0100

    Made a blueprint for making more enemies

--- Enemies/EnemyList.cs
+++ Enemies/EnemyList.cs
@@ -4,6 +4,24 @@ namespace DiscordRPG
 {
     public class EnemyList
     {
-        public static Enemy Goblin = new Enemy("Goblin", new List<string> { "Forest" }, 5, 5, 5, 3, MaterialList.LootGen(MaterialList.goblinEarOne, 10), MaterialList.LootGen(MaterialList.goblinEarTwo, 20), MaterialList.LootGen(MaterialList.goblinGem, 5));
+        public static Enemy Goblin = new Enemy("Goblin",
+            new List<string> { "Forest" },
+            5, //attack
+            5, //defense
+            5, //health
+            3, //pulls
+            MaterialList.LootGen(MaterialList.goblinEarOne, 20),
+            MaterialList.LootGen(MaterialList.goblinEarTwo, 10),
+            MaterialList.LootGen(MaterialList.goblinGem, 5));
+
+        public static Enemy Slime = new Enemy("Slime",
+            new List<string> { "Forest" },
+            4, //attack
+            6, //defense
+            6, //health
+            3, //pulls
+            MaterialList.LootGen(MaterialList.slimeGooOne, 20),
+            MaterialList.LootGen(MaterialList.slimeGooTwo, 10),
+            MaterialList.LootGen(MaterialList.slimeGem, 5));
     }
 }
--- Loot/Materials/MaterialList.cs
+++ Loot/Materials/MaterialList.cs
@@ -6,6 +6,10 @@
         public static Material goblinEarTwo = new Material("Goblin Ear", 2, 1, "Normal");
         public static Material goblinGem = new Material("Goblin Gem", 1, 2, "Magic");
 
+        public static Material slimeGooOne = new Material("Slime Goo", 1, 1, "Normal");
+        public static Material slimeGooTwo = new Material("Slime Goo", 2, 1, "Normal");
+        public static Material slimeGem = new Material("Slime Gem", 1, 2, "Magic");
+
         public static Material LootGen(Material material, int chance)
         {
             material.Chance = chance;
