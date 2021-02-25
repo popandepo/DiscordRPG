e202f7a75715ff3c9d7850e0376df26de7457d03
popandepo <popandepo@gmail.com>
2021-02-25 20:03:34 +0100

    added Emojis.cs from old

---
+++ Tools/Emojis.cs
@@ -0,0 +1,33 @@
+﻿using Discord;
+
+namespace DiscordRPG
+{
+    class Emojis
+    {
+        public static Emoji Shield = new Emoji("🛡️");
+        public static Emoji Sword = new Emoji("⚔️");
+        public static Emoji Bag = new Emoji("💼");
+        public static Emoji Wand = new Emoji("🪄");
+        public static Emoji QuestionMark = new Emoji("❓");
+        public static Emoji TurnBack = new Emoji("↩️");
+        public static Emoji CheckMark = new Emoji("✅");
+        public static Emoji CrossMark = new Emoji("❎");
+        public static Emoji BackButton = new Emoji("◀️");
+        public static Emoji NextButton = new Emoji("▶️");
+        public static Emoji Flag = new Emoji("🏁");
+        public static Emoji[] Numbers = { //Access using index
+            new Emoji("0️⃣"),
+            new Emoji("1️⃣"),
+            new Emoji("2️⃣"),
+            new Emoji("3️⃣"),
+            new Emoji("4️⃣"),
+            new Emoji("5️⃣"),
+            new Emoji("6️⃣"),
+            new Emoji("7️⃣"),
+            new Emoji("8️⃣"),
+            new Emoji("9️⃣"),
+            new Emoji("🔟")
+        };
+
+    }
+}
